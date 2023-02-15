ec0a8cc5a7d2e92ce617edd58a218ee27699b8b2
Juha <viderus@gmail.com>
2023-02-15 15:04:44 +0200

    added weapon class which is extending item

--- src/main/java/items/Weapon.java
+++ src/main/java/items/Weapon.java
@@ -1,4 +1,36 @@
 package items;
 
-public class Weapon {
+public class Weapon extends Item {
+    private WeaponType weaponType;
+    private int damage;
+
+    enum WeaponType {
+        AXE,
+        BOW,
+        DAGGER,
+        HAMMER,
+        STAFF,
+        SWORD,
+        WAND,
+    }
+    public Weapon(String name, Integer requiredLevel, Slot slot, WeaponType weaponType, int damage){
+        this.setName(name);
+        this.setRequiredLevel(requiredLevel);
+        this.damage = damage;
+        this.weaponType = weaponType;
+        this.setSlot(Slot.WEAPON);
+    }
+
+    public WeaponType getWeaponType() {
+        return weaponType;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
 }
