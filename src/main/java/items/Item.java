ce8dd5dc8dadabe2cb05bad664bae331d146536d
Juha <viderus@gmail.com>
2023-02-15 15:05:25 +0200

    added item class

--- src/main/java/items/Item.java
+++ src/main/java/items/Item.java
@@ -1,4 +1,38 @@
 package items;
 
 public class Item {
+    private String name;
+    private int requiredLevel;
+    private Slot slot;
+
+    enum Slot {
+        WEAPON,
+        HEAD,
+        BODY,
+        LEGS,
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Slot getSlot() {
+        return slot;
+    }
+
+    public void setSlot(Slot slot) {
+        this.slot = slot;
+    }
+
+    public void setRequiredLevel(int requiredLevel) {
+        this.requiredLevel = requiredLevel;
+    }
+
+    public int getRequiredLevel() {
+        return requiredLevel;
+    }
 }
