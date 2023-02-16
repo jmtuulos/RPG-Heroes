f87645bbab9d08261cf5d7fca662b00cf3973915
Juha <viderus@gmail.com>
2023-02-16 17:48:14 +0200

    finished mage class with exception handling

--- src/main/java/heroes/Mage.java
+++ src/main/java/heroes/Mage.java
@@ -1,6 +1,8 @@
 package heroes;
 
 import attributes.HeroAttribute;
+import exceptions.InvalidArmorException;
+import exceptions.InvalidWeaponException;
 import items.Armor;
 import items.Weapon;
 
@@ -15,18 +17,29 @@ public class Mage extends Hero {
     @Override
     public void levelUp() {
         this.setLevel(this.getLevel() + 1);
-        this.setStartAttributes(1, 1, 5);
-
+        this.setStartAttributes(5, 1, 1);
     }
 
     @Override
     public boolean equipItem(Armor armor) throws InvalidArmorException {
-        return false;
+        if (armor.getRequiredLevel() > this.getLevel())
+            throw new InvalidArmorException("You cannot equip this item, (lvl " + armor.getRequiredLevel() + " required)");
+        if (armor.getArmorType() != Armor.ArmorType.CLOTH)
+            throw new InvalidArmorException("Not a valid armor for " + getHeroType());
+        this.getEquipment().put(armor.getSlot(), armor);
+        this.setTotalAttributes(armor.getArmorAttribute().getStrength(), armor.getArmorAttribute().getDexterity(), armor.getArmorAttribute().getIntelligence());
+        this.damageAttribute = this.getTotal().getIntelligence();
+        return true;
     }
 
     @Override
     public boolean equipItem(Weapon weapon) throws InvalidWeaponException {
-        return false;
+        if (weapon.getRequiredLevel() > this.getLevel())
+            throw new InvalidWeaponException("You cannot equip this item, (lvl " + weapon.getRequiredLevel() + " required)");
+        else if (weapon.getWeaponType() != Weapon.WeaponType.STAFF && weapon.getWeaponType() != Weapon.WeaponType.WAND)
+            throw new InvalidWeaponException("Not a valid weapon for Mage");
+        this.getEquipment().put(weapon.getSlot(), weapon);
+        return true;
     }
 
     @Override
